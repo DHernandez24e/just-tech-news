282177753d84f646263688990ff47d7f47f7a3a0
Doggo <dhernandez24e@hotmail.com>
2020-06-05 19:18:46 -0700

    add vote routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -17,12 +17,25 @@ router.get('/', (req, res) => {
 //GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ],
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         }
     })
         .then(dbUserData => {
+            // console.log(dbUserData);
             if (!dbUserData) {
                 res.status(400).json({ message: 'No user found with this id '});
                 return;
